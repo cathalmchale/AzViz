a59f5cd913ba6aa8b52cf1f6b8ad5e505a2ba45c
Cathal Mchale <cathal.mchale@computacenter.com>
2025-10-22 10:17:31 +0100

    debug entry point

--- Program.ps1
+++ Program.ps1
@@ -10,6 +10,6 @@
 Remove-Module AzViz -ErrorAction SilentlyContinue
 Import-Module AzViz -Scope Local -Verbose
 
-$debugRg = @('rg-core-pro')
-$excludeTypes = @("*slots","*smartDetectorAlertRules","*disks","*snapshots","*certificates","*privateDnsZones")
+$debugRg = @('rg-report-pro','rg-apishare-pro')
+$excludeTypes = @("*slots") #@("*slots","*smartDetectorAlertRules","*disks","*snapshots","*certificates","*privateDnsZones")
 Export-AzViz -ResourceGroup $debugRg -ExcludeTypes $excludeTypes -Theme light -OutputFormat png -Show -LabelVerbosity 2 -SubnetResourceRowLimit 2 -SkipNetwork
